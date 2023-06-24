bd84c444c94570313305630d7af44462a449b7e5
FelipeWanner <felipe.w.pakua@gmail.com>
2023-06-24 21:34:49 +1000

    adding validation function

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 function playSound(audioId){
-    document.querySelector(audioId).play();
+    const element = document.querySelector(audioId);
+
+    if(element === null){
+        console.log("Element can't be found")
+    } else {
+        if(element.localName === 'audio'){
+            element.play();
+        }
+    }
+
 }
 
 const keyList = document.querySelectorAll(".key");
@@ -10,15 +19,23 @@ let i = 0
 // for every key. So when the user clicks on any key, 
 // the while will atribute a function to every key, and the key he pressed will trigger the 'onclick' method
 
-while (i < keyList.length){
+for (i; i < keyList.length; i++){
 
     const key = keyList[i]; //select every key from the keylist
     const instrument = key.classList[1]; //select the second class given in the HTML for every key
     const idAudio = `#sound_${instrument}`; //here we are creating the ID's that exists on the HTML document
-    console.log(idAudio)
-    key.onclick = function () { //opening an anonymous function, since we cant call a function when opening the browser.
+    
+    key.onclick = function () { //opening an anonymous function, since we cant call a function when the user open the browser
         playSound(idAudio); //finally, we are calling the playSound function giving the ID to it.
     }
 
-    i++
+    key.onkeydown = function (event) {
+        if(event.code === 'Space' || event.code === 'Enter'){
+            key.classList.add('ativa');
+        }
+    }
+
+    key.onkeyup = function () {
+        key.classList.remove("ativa");
+    }
 }
